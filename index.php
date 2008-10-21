0255bea6de901bb40d79ae70c937cf770e4c248b
root <root@mint.(none)>
2008-10-21 06:50:57 -0500

    Initial commit of files in /

---
+++ index.php
@@ -0,0 +1,119 @@
+<?PHP
+
+include 'functions.inc';
+
+$GLOBALS['MenuLinks'] = array(
+   array('Name' => 'Software Projects and Downloads',
+	 'URL' => '/software/',
+	 'Links' => array(
+	    array('Name' => 'D&D Helper',
+		  'Desc' => 'Palm OS software to help speed up your ' .
+		     'Dungeons and Dragons campaign (or other type of ' .
+		     'dice-based system) by rolling dice, looking up ' .
+		     'information, and generating interesting things.',
+		  'URL' => '/software/dnd_helper/'),
+	    array('Name' => 'Java Puzzle Applet',
+		  'Desc' => 'Free puzzle applet that lets you easily add ' .
+		     'an image puzzle to your web site.  Very ' .
+		     'customizable.',
+		  'URL' => '/software/puzzle/'),
+	    array('Name' => 'Marco',
+		  'Desc' => 'A set of surveyor programs that run on Palm ' .
+		     'OS, intended to replace outdated hardware and bulky ' .
+		     'laptops in the field.',
+		  'URL' => '/software/marco/'),
+	 )),
+   array('Name' => 'Fun',
+	 'URL' => '/fun/',
+	 'Links' => array(
+	    array('Name' => 'Interactive Games',
+		  'Desc' => 'Java applets and other things that I have ' .
+		     'found elsewhere',
+		  'URL' => '/fun/games/'),
+	    array('Name' => 'Personality Tests',
+		  'Desc' => 'Fun little multiple-choice questions ' .
+		     'that are intended to reveal information about ' .
+		     'yourself that you never thought of before.',
+		  'URL' => '/fun/tests/'),
+	    array('Name' => 'Trivia and Fun Facts',
+		  'Desc' => 'An archive of the amazing trivia, facts, ' .
+		     'and the occasional quotes that are ' .
+		     'put up on a whiteboard at my work.',
+		  'URL' => '/fun/trivia/'),
+	 )),
+   array('Name' => 'Reference Materials',
+	 'URL' => '/reference/',
+	 'Links' => array(
+	    array('Name' => 'D&D Resources',
+		  'Desc' => 'House rules and software used by our D&D ' .
+		     'sessions.',
+		  'URL' => '/reference/dnd/'),
+	    array('Name' => 'Email Information for Rumkin.com',
+		  'Desc' => 'Useful information for people who use this ' .
+		     'machine as their email server.',
+		  'URL' => '/reference/email/'),
+	    array('Name' => 'Palm OS Programming',
+		  'LongName' => 'Palm OS',
+		  'Desc' => 'Free programs, links to useful sites, etc.',
+		  'URL' => '/reference/palm/'),
+         )),
+   array('Name' => 'Web-Based Tools',
+	 'URL' => '/tools/',
+	 'Links' => array(
+	    array('Name' => 'Sprint PCS Vision Uploader',
+		  'Desc' => 'Send new ringtones, images, and Java midlets ' .
+		     'to your phone with ease.  Also works with many ' .
+		     'phones from other providers.',
+		  'URL' => '/tools/sprint/'),
+	    array('Name' => 'SSH Applet',
+		  'Desc' => 'Need to log into a remote machine but you ' .
+		     'don\'t have an SSH client handy?  On a loaner ' .
+		     'computer or can\'t install software?  Just need to ' .
+		     'SCP that one file?  Here you go!',
+		  'URL' => '/tools/ssh/'),
+	    array('Name' => 'Mailto Encoder',
+		  'Desc' => 'A safe way to put your email address on a web ' .
+		     'page and not have it harvested by spambots!',
+		  'URL' => '/tools/mailto_encoder/'),
+	    array('Name' => 'PJIRC',
+		  'Desc' => 'Java IRC applet, for those times that you ' .
+		     'need to chat and you are away from your normal IRC ' .
+		     'client.',
+		  'URL' => '/tools/pjirc/'),
+         )),
+   );
+
+
+StandardHeader(array('title' => 'Rumkin.com'));
+
+?>
+
+<dl>
+
+<?PHP
+
+global $MenuLinks;
+
+foreach ($MenuLinks as $Menu) {
+   echo '<dt><b><a href="' . $Menu['URL'] . '">';
+   if (isset($Menu['LongName']))
+      echo htmlspecialchars($Menu['LongName']);
+   else
+      echo htmlspecialchars($Menu['Name']);
+   echo ":</a></b></dt>\n";
+   
+   foreach ($Menu['Links'] as $Sub) {
+      echo '<dd><a href="' . $Sub['URL'] . '">';
+      if (isset($Sub['LongName']))
+         echo htmlspecialchars($Sub['LongName']);
+      else
+         echo htmlspecialchars($Sub['Name']);
+      echo '</a> - ' . htmlspecialchars($Sub['Desc']) . "</dd>\n";
+   }
+   echo '<dd><font size="-1">(<a href="' . $Menu['URL'] . 
+      '">More ...</a>)</font></dd>' . "\n";
+}
+
+echo "</dl>\n";
+
+StandardFooter();
---
+++ loan.php
@@ -0,0 +1,123 @@
+<?PHP
+
+$StartMonth = 4;
+$StartYear = 2001;
+$StartDue = 21500;
+$Payments = array(
+// JAN  FEB  MAR  APR  MAY  JUN  JUL  AUG  SEP   OCT   NOV  DEC
+                  125,   0,   0,   0,   0, -500,   0, 7288, 250, // 2001
+   500,   0,   0,   0,   0,   0,   0,   0, 1500,   0,    0, 635, // 2002
+  2500,   0,   0,   0,   0, 200, 200,   0,  250,   0,  270,   0, // 2003
+   200, 200, 200, 500, 200, 400,   0, 200,  200, 200,  200,   0, // 2004
+  1500,   0,   0,   0, 200,   0, 200,   0,    0, 200,    0,   0, // 2005
+     0, 700); // 2006
+
+if (! isset($EstimatePayment))
+   $EstimatePayment = 200;
+
+$Months = array('', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+   'Sep', 'Oct', 'Nov', 'Dec');
+
+$Numbers = array();
+foreach ($Payments as $Payment) {
+   $Number['Month'] = $Months[$StartMonth];
+   $Number['Year'] = $StartYear;
+   $StartMonth ++;
+   if ($StartMonth > 12) {
+      $StartYear ++;
+      $StartMonth = 1;
+   }
+   $Number['Due'] = $StartDue;
+   $Number['Interest'] = ($StartDue * 0.07) / 12;
+   $Number['Interest'] = round($Number['Interest'], 2);
+   $Number['Paid'] = $Payment;
+   $Number['Left'] = round($StartDue - $Payment + $Number['Interest'], 2);
+   $StartDue = $Number['Left'];
+   
+   array_unshift($Numbers, $Number);
+}
+
+?><html><head><title>Pay Off Dad's Loan</title>
+</head>
+<body bgcolor="#FFFFFF">
+
+<P><B>Notes:</b> Pay on the first of the month.  Payments made after that
+day count towards the next month.</p>
+
+<?PHP ShowTable('History', $Numbers); ?>
+
+<form method=post action=loan.php>
+<p><b>Estimate if payment is
+$<input type=text name=EstimatePayment value="<?PHP echo $EstimatePayment ?>"
+size=5><input type=submit value="Change"> - 
+<input type=checkbox name="SeeFull"<?PHP 
+if (isset($SeeFull)) echo " CHECKED"; ?>> See all estimate entries
+</b></p>
+</form>
+
+<?PHP
+
+if ($EstimatePayment > round(($StartDue * 0.07) / 12, 2)) {
+   $Estimate = array();
+   $monthNum = 0;
+   while ($StartDue && ($monthNum < 96 || isset($SeeFull))) {
+      $monthNum ++;
+      $Number['Month'] = $Months[$StartMonth];
+      $Number['Year'] = $StartYear;
+      $StartMonth ++;
+      if ($StartMonth > 12) {
+         $StartYear ++;
+         $StartMonth = 1;
+      }
+      $Number['Due'] = $StartDue;
+      $Number['Interest'] = ($StartDue * 0.07) / 12;
+      $Number['Interest'] = round($Number['Interest'], 2);
+      if ($StartDue + $Number['Interest'] < $EstimatePayment)
+         $EstimatePayment = $StartDue + $Number['Interest'];
+      $Number['Paid'] = $EstimatePayment;
+      $Number['Left'] = round($StartDue + $Number['Interest'] - 
+                              $EstimatePayment, 2);
+      $StartDue = $Number['Left'];
+   
+      $Estimate[] = $Number;
+   }
+   ShowTable('Estimate', $Estimate);
+} else {
+   echo "<p><b>$EstimatePayment is not large enough!</b>  It must be $";
+   echo round(($StartDue * 0.07) / 12, 2) + 0.01;
+   echo " or larger.</p>\n";
+}
+
+?>
+</body>
+</html>
+<?PHP
+
+function ShowTable($Title, $Numbers) {
+?><h1><?= $Title ?></h1>
+<table border=1>
+<tr><th>Month / Year</th><th>Due</th><th>Interest</th><th>Paid</th>
+<th>Left</th></tr>
+<?PHP
+
+foreach ($Numbers as $Number) {
+   echo "<tr><td align=center>" . $Number['Month'] . " " .
+      $Number['Year'] . "</td><td align=center>$ " .
+      ShowNumber($Number['Due']) .
+      "</td><td align=center>$ " . ShowNumber($Number['Interest']) .
+      "</td><td align=center>$ " . $Number['Paid'] .
+      "</td><td align=center>$ " . ShowNumber($Number['Left']) . 
+      "</td></tr>\n";
+}
+
+echo "</table>\n";
+}
+
+function ShowNumber($Number) {
+   settype($Number, 'string');
+   if (strpos($Number, '.') === false)
+      $Number .= '.';
+   while (! ereg('\.[0-9][0-9]', $Number))
+      $Number .= '0';
+   return $Number;
+}
