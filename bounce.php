73659db8e185cfdf2706e9190cd7034f3bb2599e
root <root@mint.(none)>
2008-10-21 06:52:36 -0500

    Initial commit of more files in /

---
+++ bounce.php
@@ -0,0 +1,10 @@
+<?PHP
+
+include_once('inc/util.php');
+
+$target = $_SERVER['REQUEST_URI'];
+$target = substr($target, strlen($_SERVER['SCRIPT_NAME']) + 1);
+if ($target == '')
+  Redirect('http://rumkin.com/');
+
+Redirect($target);
