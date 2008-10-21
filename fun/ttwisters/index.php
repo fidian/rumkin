a7be4ada6eaea9e28ec42b82d35f027e393642a1
root <root@mint.(none)>
2008-10-21 06:40:55 -0500

    Initial commit of /fun/ttwisters/

---
+++ fun/ttwisters/index.php
@@ -0,0 +1,34 @@
+<?PHP
+
+include '../../functions.inc';
+
+StandardHeader(array('title' => 'Tongue Twisters',
+		     'topic' => 'ttwisters'));
+
+?>
+
+<p>Tongue twisters are fun little exercises for your mouth.  They often use
+alliteration (the repeating of the beginning sounds of words) and rhyme in
+order to force you to verbally "stumble" instead of speaking clearly.
+The mistake will usually produce a smile in those listening to your 
+attempt.</p>
+
+<p>You can check out the various tongue twisters on the other pages in this
+section.  A good place to start is with the "5 Times Fast" category.</p>
+
+<p>If you know of a tongue twister that you would like to see listed on
+these pages, just type it into the chat box on the bottom of any of these
+pages.  Please make sure it is not already listed before suggesting that
+I add it.</p>
+	
+<?PHP Section('Categories') ?>
+	
+<ul>
+<li><a href="5times.php">5 Times Fast</a></li>
+<li><a href="oneline.php">One-Liners</a></li>
+<li><a href="verse.php">Story and Verse</a></li>
+</ul>
+
+<?PHP
+
+StandardFooter();
---
+++ fun/ttwisters/verse.php
@@ -0,0 +1,151 @@
+<?PHP
+
+include '../../functions.inc';
+
+StandardHeader(array('title' => 'Verse',
+		     'heading' => 'Verse Tongue Twisters',
+		     'topic' => 'ttwisters'));
+
+?>
+
+<p>These tongue twisters are little stories or fun rhymes that are difficult
+to say.  There are numerous versions of some of these, and each region has
+its own unique variations.  Check out the links for other tongue twister
+pages.</p>
+
+
+<?PHP Section('Verse'); ?>
+
+<p>A tutor who tooted the flute<br>
+Tried to tutor two tutors to toot.<br>
+Said the two to the tutor,<br>
+"Is it harder to toot,<br>
+Or to tutor two tutors to toot?"</p>
+
+<p>"Lift the ladder later," lisped Lester.<br>
+Lester lisped, "Lift the latter later."</p>
+
+<p>Peter Piper picked a peck of pickled peppers.<br>
+A peck of pickled peppers Peter Piper picked.<br>
+If Peter Piper picked a peck of pickled peppers,<br>
+Where's the peck of pickeled peppers Peter Piper picked?</p>
+
+<p>I stood on the steps of Burgesses fish sauce shop<br>
+Mimicking him hiccuping and welcoming him in.</p>
+
+<p>Moses supposes his toses are roses,<br>
+But Moses supposes erroneously,<br>
+For nobody's toeses are posies of roses<br>
+As Moses supposes his toses to be.<br>
+<i>[Pronounce toses as "toe-zez" so it rhymes with Moses]</i></p>
+
+<p>Tom threw Tim three thumbtacks.<br>
+Three thumbtacks were thrown to Tim by Tom.<br>
+Why did Tom throw Tim three thumbtacks?</p>
+
+<p>Three gray geese in the green grass grazing.<br>
+Gray were the geese and green was the grass<br>
+In which the geese were grazing.</p>
+
+<p>A skunk sat on a stump.<br>
+The stump thunk the skunk stunk.<br>
+The skunk thunk the stump stunk.</p>
+
+<p>Theophilus, the thistle sifter, while sifting a sifter full of thistles,
+thrust three thousand thistles through the thick of his thumb.</p>
+
+<p>Betty Bottter bought some butter,<br>
+"But," she said, "the butter's bitter.<br>
+If I put it in my batter,<br>
+It will make my batter better."<br>
+So, she bought a bit of butter<br>
+And she put it in her batter<br>
+And it made her batter better.<br>
+(Shouldn't bitter butter make Betty Botter's batter bitter?<br>
+Or is bitter batter from bitter butter better?)</p>
+
+<p>Betty Botter had some butter,<br>
+"But," she said, "the butter's bitter.<br>
+If I bake this bitter butter,<br>
+It would make my batter bitter.<br>
+But a bit of better butter,<br>
+That would make my batter better."<br>
+So she bought a bit of butter,<br>
+Better than her bitter butter,<br>
+And she baked it in her batter;<br>
+And the batter was not bitter<br>
+So 'twas better Betty Botter<br>
+Bought a bit of better butter.</p>
+
+<p>How much wood would a woodchuck chuck if a woodchuck could chuck wood?<br>
+A woodchuck would chuck as much wood as a woodchuck could if a woodchuck
+could chuck wood.</p>
+
+<p>If one doctor doctors another doctor, does the doctor who doctors the 
+doctor doctor the doctor the way the doctor he is doctoring doctors?
+Or does he doctor the doctor the way the doctor who doctors doctors?<br>
+(The doctoring doctor doctors the doctor the way the doctoring doctor wants
+to doctor the doctor; not the way the doctored doctor wants to be
+doctored.)</p>
+
+<p>A tree toad loved a she-toad who lived up in a tree.<br>
+He was a two-toed tree toad but a three-toed was she.<br>
+The two-toed tree toad tried to win the three-ted she-toad's heart,<br>
+For the two-toed tree toad loved the ground that the three-toed tree toad
+trod.<br>
+But the two-toed tree toad tried in vain.  He couldn't please her whim.<br>
+From her tree toad bower with her three-toed power the she-toad vetoed
+him.</p>
+
+<p>A flea and a fly in a flue<br>
+Said the fly, "Oh what should we do?"<br>
+Said the flea, "Let us fly."<br>
+Said the fly, "Let us flee."<br>
+So they flew through a flaw in the flue.</p>
+
+<p>Mr. See owned a saw and Mr. Soar owned a seesaw.<br>
+Now, See's saw sawed Soar's seesaw before Soar saw See, which made Soar
+sore.<br>
+Had Soar seen See's saw before See sawed Soar's seesaw,<br>
+See's saw would not have sawed Soar's seesaw.<br>
+So See's saw sawed Soar's seesaw.<br>
+But it was sad to see Soar so sore just because See's saw sawed Soar's
+seesaw.</p>
+
+<p>Once a fellow met a fellow in a field of beans.<br>
+Sed a fellow to a fellow, "If a fellow asks a fellow,<br>
+Can a fellow tell a fellow what a fellow means?"</p>
+
+<p>I slit a sheet, a sheet I slit.<br>
+And on a slitted sheet I sit.<br>
+I slit a sheet, a sheet I slit.<br>
+The sheet I slit, that sheet was it.</p>
+
+<p>Swan swam over the sea,<br>
+Swim, swan, swim!<br>
+Swan swam back again,<br>
+Well swum, swan!</p>
+
+<p>Silly Sally swiftly shooed seven silly sheep.<br>
+The seven silly sheep Silly Sally shooed shilly-shallied south.<br>
+These sheep shouldn't sleep in a shack;<br>
+Sheep should sleep in a shed.</p>
+
+<p>You've no need to light a night-light<br>
+On a light night like tonight,<br>
+For a night-light's light's a slight light,<br>
+And tonight's a night that's light.<br>
+When a night's light, like tonight's light,<br>
+It is really not quite right<br>
+To light night-lights with their slight lights<br>
+On a light night like tonight.</p>
+	
+<p>One-One was a racehorse.<br>
+Two-Two was one too.<br>
+When One-One won one race,<br>
+Two-two won one too.</p>
+
+
+<?PHP
+
+StandardFooter();
