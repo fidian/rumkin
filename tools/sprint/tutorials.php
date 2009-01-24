0559277fc33c72e5bda9e149d2ea6fe74c1f9ca0
Tyler Akins <fidian@rumkin.com>
2009-01-23 22:47:13 -0600

    Added the videos from JarrodJS

---
+++ tools/sprint/tutorials.php
@@ -0,0 +1,43 @@
+<?php
+include 'common.inc';
+SprintStandardHeader('Phone Uploader');
+
+?>
+
+<p>I greatly appreciate the work that <a href="http://www.youtube.com/user/JarrodJS">JarrodJS</a> has put into making some videos on how to use this site.</p>
+
+<?PHP
+
+Section('Wallpapers');
+YouTube('Mo6TOw0EqK4', array('float' => 'right')); // wallpapers
+
+?>
+
+<ol>
+<li>Find a picture and save it on your computer.
+<li>Resize it to fit your phone's screen.  The uploader will do this automatically for you on some phones.
+<li>Upload it to the uploader.
+<li>Use your phone's browser to go to the jump page.
+<li>Enter the jump code.
+<li>Download to your phone.  If things work out well, you should now be able to set it as a wallpaper somehow.
+</ol>
+
+<?PHP
+
+Section('Applications');
+YouTube('tj6Ejg2ZHLo', array('float' => 'right'));  // app
+
+?>
+
+<ol>
+<li>Find an application for your phone and save it on your computer.
+<li>Upload it to the uploader.
+<li>Use your phone's browser to go to the jump page.
+<li>Enter the jump code.
+<li>Download the program to your phone.
+<li>Test the program.  It might not work for your phone, so don't get too upset if there are problems.
+</ol>
+
+<?PHP
+
+StandardFooter();
