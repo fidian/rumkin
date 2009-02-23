38bfe0fa8773bfeece7058894449ef7643cdc109
Tyler Akins <fidian@rumkin.com>
2009-02-23 09:28:10 -0600

    Added two more videos

--- tools/sprint/tutorials.php
+++ tools/sprint/tutorials.php
@@ -19,7 +19,23 @@ YouTube('Mo6TOw0EqK4', array('float' => 'right')); // wallpapers
 <li>Upload it to the uploader.
 <li>Use your phone's browser to go to the jump page.
 <li>Enter the jump code.
-<li>Download to your phone.  If things work out well, you should now be able to set it as a wallpaper somehow.
+<li>Download to your phone.  If things work out well, you should now be able to set it as a wallpaper somehow.  Otherwise, check out the <a href="/tools/decision_tree/index.php?Tree=uploader">problems troubleshooter</a>.
+</ol>
+
+<?PHP
+
+Section('Scaling Down Images');
+YouTube('8X2JPFw93YM', array('float' => 'right'));  // scale down images
+
+?>
+
+<ol>
+<li>Find a picture from your digital camera and load it in Paint.
+<li>Resize it smaller so it loads much faster.  <i>NOTE:  You may need to resize and crop it so it exactly fits your phone.  That is not covered in this video.</i>
+<li>Upload it to the uploader.
+<li>Use your phone's browser to go to the jump page.
+<li>Enter the jump code.
+<li>Download the smaller image to your phone.  Depending on your phone, you may experience some <a href="/tools/decision_tree/index.php?Tree=uploader">issues</a>
 </ol>
 
 <?PHP
@@ -35,7 +51,23 @@ YouTube('tj6Ejg2ZHLo', array('float' => 'right'));  // app
 <li>Use your phone's browser to go to the jump page.
 <li>Enter the jump code.
 <li>Download the program to your phone.
-<li>Test the program.  It might not work for your phone, so don't get too upset if there are problems.
+<li>Test the program.  It might not work for your phone, so don't get too upset if there are <a href="/tools/decision_tree/index.php?Tree=uploader">problems</a>.
+</ol>
+
+<?PHP
+
+Section('Uploading Ringtones');
+YouTube('vUYT-6e6pAA', array('float' => 'right'));  // upload ringtone
+
+?>
+
+<ol>
+<li>Go find a song or ringer and save it on your computer.
+<li>Upload it to the uploader.
+<li>Use your phone's browser to go to the jump page.
+<li>Enter the jump code.
+<li>Download the ringer to your phone.
+<li>Assign the ringer to a contact or assign it as your default ringer.  Your phone may not be so friendly - see the <a href="/tools/decision_tree/index.php?Tree=uploader">problems troubleshooter</a> if there are issues.
 </ol>
 
 <?PHP
