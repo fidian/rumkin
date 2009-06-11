6dd3236c4c9a9377f824456ece1ca368933761d2
Tyler Akins <fidian@rumkin.com>
2009-06-11 18:36:31 -0500

    Multiple images on one page + pretty print

--- fun/images/index.php
+++ fun/images/index.php
@@ -16,10 +16,18 @@ $next = ($_REQUEST['id'] + 1) % count($Images);
 $prev = ($_REQUEST['id'] - 1 + count($Images)) % count($Images);
 $random = rand(0, count($Images) - 1);
 echo '<div align="center">';
-echo '<p><a href="?id=' . $prev . '">Prev</a> - ' . '<a href="?id=' . $random . '">Random</a> - ' . '<a href="?id=' . $next . '">Next</a></p>';
-echo '<img src="media/' . $Image . '">';
+echo '<p><a href="?id=' . $prev . '">Prev</a> - <a href="?id=' . $random . '">Random</a> - <a href="?id=' . $next . '">Next</a></p>';
+
+if (! is_array($Image)) {
+	echo '<img src="media/' . $Image . '">';
+} else {
+	foreach ($Image as $file) {
+		echo '<img src="media/' . $file . '"><br>';
+	}
+}
+
 echo '</div>';
-$DescName = preg_replace('/\.(jpg|gif)$/i', '.txt', $Image);
+$DescName = preg_replace('/[0-9]*\.(jpg|gif)$/i', '.txt', $Image);
 
 if (file_exists($DescName)) {
 	echo '<br>';
@@ -39,15 +47,25 @@ function MyScanDir($dirname) {
 		if ($file[0] != '.') {
 			$fullname = $dirname . '/' . $file;
 			
-			if (is_dir($fullname)) {
-				$More = MyScanDir($fullname);
-				$Images = array_merge($Images, $More);
-			} elseif (preg_match('/\.(jpg|gif)$/i', $file)) {
-				$Images[] = $file;
+			if (preg_match('/(.*[^0-9])([0-9]*)\.(jpg|gif)$/i', $file, $matches)) {
+				$key = $matches[1];
+				
+				if (isset($Images[$key])) {
+					if (! is_array($Images[$key])) {
+						$Images[$key] = array(
+							$Images[$key]
+						);
+					}
+					
+					$Images[$key][] = $file;
+				} else {
+					$Images[$key] = $file;
+				}
 			}
 		}
 	}
 	
-	return $Images;
+	ksort($Images);
+	return array_values($Images);
 }
 
