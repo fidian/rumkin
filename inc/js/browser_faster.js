391d443dc8775bd00adaf0d0bb48d3c3131b232d
Tyler Akins <fidian@rumkin.com>
2009-10-16 16:37:36 -0500

    Better browser detection, length, prepend

--- inc/js/browser_faster.js
+++ inc/js/browser_faster.js
@@ -1,9 +1,15 @@
 var ua = navigator.userAgent.toLowerCase();
-if (ua.indexOf("chrome") >= 0 || ua.indexOf("firefox") >= 0) {
+if (ua.indexOf(" chrome/") >= 0 || ua.indexOf(" firefox/") >= 0 || ua.indexOf(' gecko/') >= 0) {
 	var StringMaker = function () {
 		this.str = "";
+		this.length = 0;
 		this.append = function (s) {
 			this.str += s;
+			this.length += s.length;
+		}
+		this.prepend = function (s) {
+			this.str = s + this.str;
+			this.length += s.length;
 		}
 		this.toString = function () {
 			return this.str;
@@ -12,8 +18,14 @@ if (ua.indexOf("chrome") >= 0 || ua.indexOf("firefox") >= 0) {
 } else {
 	var StringMaker = function () {
 		this.parts = [];
+		this.length = 0;
 		this.append = function (s) {
 			this.parts.push(s);
+			this.length += s.length;
+		}
+		this.prepend = function (s) {
+			this.parts.unshift(s);
+			this.length += s.length;
 		}
 		this.toString = function () {
 			return this.parts.join('');
