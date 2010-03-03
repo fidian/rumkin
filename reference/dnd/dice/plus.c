b8596fb6d72b6646d562d81eac016ede21744f90
Tyler Akins <fidian@rumkin.com>
2010-03-03 08:11:56 -0600

    Made more generic

--- reference/dnd/dice/plus.c
+++ reference/dnd/dice/plus.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void showRolls(int dice, int *rolls) {
+	int i;
+
+	for (i = 0; i < dice; i ++) {
+		if (i) {
+			printf(", ");
+		}
+		printf("%d", rolls[i]);
+	}
+
+	printf("\n");
+}
+
 int main(int argc, char **argv) {
 	unsigned long long *tallies;
 	int *rolls;
-	int sides, dice, i, tally, max;
+	int sides, dice, i, duplicates, tally, maxRoll, maxTallies;
 
 	if (argc < 3) {
 		printf("Specify number of sides and dice\n");
@@ -20,33 +33,35 @@ int main(int argc, char **argv) {
 	}
 
 	printf("Sides %d, dice %d\n", sides, dice);
+	maxTallies = sides + (2 * dice) - 1;
 
-	tallies = (unsigned long long *) malloc(sizeof(unsigned long long) * (sides + dice));
+	tallies = (unsigned long long *) malloc(sizeof(unsigned long long) * maxTallies);
 	rolls = (int *) malloc(sizeof(int) * (dice));
 
 	for (i = 0; i < dice; i ++) {
 		rolls[i] = 1;
 	}
 
-	for (i = 0; i < sides + dice; i ++) {
+	for (i = 0; i < maxTallies; i ++) {
 		tallies[i] = 0;
 	}
 
 	while (rolls[dice - 1] <= sides) {
 		// Mark the tally
-		tally = 1;
-		max = 0;
+		duplicates = 0;
+		maxRoll = 0;
 		for (i = 0; i < dice; i ++) {
-			if (rolls[i] == max) {
-				tally ++;
-			} else if (rolls[i] > max) {
-				max = rolls[i];
-				tally = 1;
+			if (rolls[i] == maxRoll) {
+				duplicates ++;
+			} else if (rolls[i] > maxRoll) {
+				maxRoll = rolls[i];
+				duplicates = 0;
 			}
 		}
 
 		// Yeah, tallies[0] will never increment
-		tallies[max + tally - 1] ++;
+		tally = maxRoll + (duplicates * 2);
+		tallies[tally] ++;
 
 		// Increment
 		i = 0;
@@ -58,7 +73,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	for (i = 1; i < sides + dice; i ++) {
+	for (i = 1; i < maxTallies; i ++) {
 		printf("%2d: %llu\n", i, tallies[i]);
 	}
 }
