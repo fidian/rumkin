e1fa7c49aaa616d7bef17904efb744ab16d4de7b
Tyler Akins <fidian@rumkin.com>
2012-11-05 16:01:38 -0600

    Removing some unnecessary files

--- bounce.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once('inc/util.php');
-$target = $_SERVER['REQUEST_URI'];
-$target = substr($target, strlen($_SERVER['SCRIPT_NAME']) + 1);
-
-if ($target == '')Redirect('http://rumkin.com/');
-Redirect($target);
