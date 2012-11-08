d105a5577c12d1cba3ff574752ba7a5aecc18430
Tyler Akins <fidian@rumkin.com>
2012-11-07 21:18:55 -0600

    Use JSON Schema and phptools

--- wcn/index.php
+++ wcn/index.php
@@ -1,29 +1,30 @@
 <?PHP
 
+require_once(__DIR__ . '/../setup.php');
+WebResponse::showErrors();
 require(__DIR__ . '/lib.php');
 
-$request = new WebRequest();
-$data = getRequestData();
+$data = json_validate_input('wcn.json');
 
 // Start to build a zip file
 $zip = new ZipBuilder();
 
-if ($data['autorun']) {
+if ($data->autorun) {
 	$zip->addFile('AUTORUN.INF', 'files/AUTORUN.INF');
 }
 
-if ($data['batch']) {
+if ($data->batch) {
 	$zip->addFile('Install_Wireless.bat', 'files/Install_Wireless.bat');
 }
 
 $zip->addFile('setupSNK.exe', 'files/setupSNK.exe');
 $zip->addDir('SMRTNTKY/', filemtime('files/SMRTNTKY/'));
 
-if ($data['autorun']) {
-	$zip->addFile('SMARTNTKY/fcw.ico', 'files/SMARTNTKY/fcw.ico');
+if ($data->autorun) {
+	$zip->addFile('SMRTNTKY/fcw.ico', 'files/SMRTNTKY/fcw.ico');
 }
 
 $zip->addFile('SMRTNTKY/MessageB.txt', 'files/SMRTNTKY/MessageB.txt');
-$zip->addFileData(buildWsettingXml(), 'SMRTNTKY/WSETTING.WFC');
-$zip->addFileData(buildWsettingTxt(), 'SMRTNTKY/WSETTING.TXT');
+$zip->addFileData(buildWsettingXml($data), 'SMRTNTKY/WSETTING.WFC');
+$zip->addFileData(buildWsettingTxt($data), 'SMRTNTKY/WSETTING.TXT');
 $zip->outputToBrowser('wcd-usb.zip');
--- wcn/lib.php
+++ wcn/lib.php
@@ -1,78 +1,38 @@
 <?PHP
 
-require(__DIR__ . '/../vendor/phptools/autoload.php');
-
-function getRequestData($request) {
-	$data = array();
-
-	$data['networkKey'] = $request->req('networkKey', '');  // Empty is allowed.
-	$data['ssid'] = $request->req('ssid', '');
-
-	// Validate input
-	$errors = array();
-	$data = array();
-
-	// All of these form values must exist.  Their values are checked later.
-	foreach (array('ssid', 'connection', 'authentication', 'encryption', 'networkkey') as $key) {
-		$data[$key] = $request->req($key, '');
-	}
-
-	// Checkboxes may not be sent, so we handle that better
-	foreach (array('automatically', 'ieee802dot1x', 'autorun', 'batch') as $key) {
-		// Force to be a boolean
-		$data[$key] = !!$request->req($key, false);
-	}
-
-	// SSID *must not* be empty
-	if (strlen($data['ssid']) == 0) {
-		$errors[] = 'SSID is empty';
-	}
-
-	$checkValid = function ($name, $allowed) use ($data, &$errors) {
-		if (! in_array($data[$name], $allowed)) {
-			$errors[] = $name . ' is not set to an allowed value';
-		}
-	};
-
-	// The rest are all part of drop-down lists
-	$checkValid('connection', array('ESS', 'IBSS'));
-	$checkValid('authentication', array('open', 'shared', 'WPA-NONE', 'WPA', 'WPAPSK', 'WPA2', 'WPA2PSK'));
-	$checkValid('encryption', array('none', 'WEP', 'TKIP', 'AES'));
-
-	if ($errors) {
-		WebResponse::status(500);
-		header('Content-type: text/plain');
-		echo "Invalid parameters\n";
-		echo "\n";
-		echo "Somehow you submitted invalid data to this service.\n";
-		echo "If this is a problem with my API, make sure to tell me about it.\n";
-		echo "\n";
-		echo "Problems:\n";
-
-		foreach ($errors as $error) {
-			echo " * $error\n";
-		}
-
-		exit();
-	}
-
-	return $data;
-}
-
-
-// Build the WSETTING.TXT file
+/**
+ * Use a template to build a text file
+ *
+ * @return string
+ */
 function buildWsettingTxt($data) {
-	$wsetting = file_get_contents('addons/WSETTING.TXT');
-	$wsetting = str_replace('##SSID##', $data['ssid'], $wsetting);
-	$wsetting = str_replace('##NETWORKKEY##', $data['networkkey'], $wsetting);
-	$wsetting = str_replace('##AUTOMATICALLY##', (int) $data['automatically'], $wsetting);
-	$wsetting = str_replace('##AUTHENTICATION##', $data['authentication'], $wsetting);
-	$wsetting = str_replace('##ENCRYPTION##', $data['encryption'], $wsetting);
-	$wsetting = str_replace('##CONNECTION##', $data['connection'], $wsetting);
-	return $wsetting;
+	$templater = new UltraLite(__DIR__ . '/files');
+	$copy = new stdclass();
+
+	foreach ($data as $name => $value) {
+		$copy->$name = $value;
+
+		// Mimic Windows
+		if (is_bool($value)) {
+			if ($value) {
+				$copy->$name = 1;
+			} else {
+				$copy->$name = 0;
+			}
+		}
+	}
+
+	$templater->data = $copy;
+	$result = $templater->render('SMRTNTKY/WSETTING.TXT');
+	return $result;
 }
 
 
+/**
+ * Build XML by hand
+ *
+ * @return string
+ */
 function buildWsettingXml($data) {
 	$xml = new DOMDocument('1.0');
 	$xml->formatOutput = true;
@@ -80,54 +40,35 @@ function buildWsettingXml($data) {
 
 	// Create the config section
 	$config = $xml->createElement('config');
-	$config->appendChild($xml->createElement('configId', makeGUID()));
+	$config->appendChild($xml->createElement('configId', guid()));
 	$config->appendChild($xml->createElement('configAuthorId', 'F46A7048-620F-7C40-779F-443FAA05003A'));
 	$config->appendChild($xml->createElement('configAuthor', 'Rumkin.com Generator at http://rumkin.com/tools/wcn/'));
 	$wp->appendChild($config);
 
 	// ssid
-	$e = $xml->createElement('ssid', $data['ssid']);
+	$e = $xml->createElement('ssid', $data->ssid);
 	$e->setAttribute('xml:space', 'preserve');
 	$wp->appendChild($e);
 
 	// connection type
-	$wp->appendChild($xml->createElement('connectionType', $data['connection']));
+	$wp->appendChild($xml->createElement('connectionType', $data->connection));
 
 	// primary profile
 	$profile = $xml->createElement('primaryProfile');
-	$profile->appendChild($xml->createElement('authentication', $data['authentication']));
-	$profile->appendChild($xml->createElement('encryption', $data['encryption']));
-	$e = $xml->createElement('networkKey', $data['networkkey']);
+	$profile->appendChild($xml->createElement('authentication', $data->authentication));
+	$profile->appendChild($xml->createElement('encryption', $data->encryption));
+	$e = $xml->createElement('networkKey', $data->networkkey);
 	$e->setAttribute('xml:space', 'preserve');
 	$profile->appendChild($e);
-	$profile->appendChild($xml->createElement('keyProvidedAutomatically', (int) $data['automatically']));
-	$profile->appendChild($xml->createElement('ieee802Dot1xEnabled', (int) $data['ieee802dot1x']));
+	$profile->appendChild($xml->createElement('keyProvidedAutomatically', (int) $data->automatically));
+	$profile->appendChild($xml->createElement('ieee802Dot1xEnabled', (int) $data->ieee802dot1x));
 	$wp->appendChild($profile);
 
-	// Finish up.
+	// Finish up
 	$xml->appendChild($wp);
 
-	// Can't validate for whatever reason, but that's ok.
-	// Microsoft's own XML doesn't validate either.
+	// Can't validate for whatever reason, but that's ok
+	// Microsoft's own XML doesn't validate either
 	$xml = $xml->saveXML();
 	return $xml;
 }
-
-
-
-
-function makeGUID() {
-	// Returns a hex string like 01234567-0123-0123-0123-0123456789AB
-	$hex = '0123456789ABCDEF';
-	$output = '';
-
-	// First, generate 8+4+4+4+12 = 32 random hex chars
-	while (strlen($output) < 32) {
-		$output .= $hex[mt_rand(0, 15)];
-	}
-
-	// Now insert hyphens
-	return substr($output, 0, 8) . '-' . substr($output, 8, 4) . '-' . substr($output, 12, 4) . '-' . substr($output, 16, 4) . '-' . substr($output, 20);
-}
-
-
