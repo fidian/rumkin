ac13aa31da7f21178658584d8d91a620833bf443
Tyler Akins <fidian@rumkin.com>
2012-12-18 08:54:44 -0600

    Initial commit of inspector

---
+++ watson/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(__DIR__ . '/../vendor/phptools/autoload.php');
+
+$request = new WebRequest();
+$sections = array();
+$sections['Cookies'] = Dump::out($request->cookie())->returned();
+$sections['GET Variables'] = Dump::out($request->get())->returned();
+$sections['POST Variables'] = Dump::out($request->post())->returned();
+
+$ul = new Ultralite(__DIR__ . '/templates');
+$ul->sections = $sections;
+echo $ul->render('index.html');
