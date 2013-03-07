811b7ce9bab70e92b33dcb0c861add12e965c413
Tyler Akins <fidian@rumkin.com>
2013-03-07 10:34:02 -0600

    Show off headers too

--- watson/index.php
+++ watson/index.php
@@ -7,6 +7,7 @@ $sections = array();
 $sections['Cookies'] = Dump::out($request->cookie())->returned();
 $sections['GET Variables'] = Dump::out($request->get())->returned();
 $sections['POST Variables'] = Dump::out($request->post())->returned();
+$sections['Request Headers'] = Dump::out(apache_request_headers())->returned();
 
 $ul = new Ultralite(__DIR__ . '/templates');
 $ul->sections = $sections;
