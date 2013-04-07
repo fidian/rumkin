7c584ffac9874d4f3c4b186d32d765f919440927
Tyler Akins <fidian@rumkin.com>
2013-04-07 11:30:18 -0500

    Update rules and fix dependency issues

--- contents/js/angular-autoloader-rules.js
+++ contents/js/angular-autoloader-rules.js
@@ -1,7 +1,8 @@
-/*global window*/
+/*global AngularAutoloader*/
 (function () {
 	'use strict';
-	window.angularAutoload = {
+	var autoloader;
+	autoloader = new AngularAutoloader({
 		"angular": {
 			"js": "/js/angular/angular.min.js",
 			"detect": [
@@ -13,6 +14,7 @@
 		"angular-ui": {
 			"js": "/js/angular-ui/build/angular-ui.min.js",
 			"css": "/js/angular-ui/build/angular-ui.min.css",
+			"module": "ui",
 			"depends": [
 				"angular",
 				"jquery"
@@ -20,11 +22,13 @@
 		},
 		"auto-grow": {
 			"js": "/js/directives/auto-grow.js",
+			"module": "autoGrow",
 			"depends": "angular",
 			"detect": "auto-grow"
 		},
 		"jquery": {
 			"js": "/js/jquery-1.9.1.min.js"
 		}
-	};
+	});
+	autoloader.init();
 }());
--- contents/js/angular-autoloader.js
+++ contents/js/angular-autoloader.js
@@ -2,6 +2,23 @@
 (function () {
 	'use strict';
 
+	var proto;
+
+	function AngularAutoloader(rules) {
+		this.foundTags = {};
+		this.neededModules = {};
+		this.loadedModules = {};
+		this.rules = rules;
+		this.numberLoading = 0;
+		this.angularModules = [];
+	}
+
+	proto = AngularAutoloader.prototype;
+
+	/**
+	 * Utility functions
+	 */
+
 	function iterate(arr, callback) {
 		var i, len;
 
@@ -18,106 +35,221 @@
 		}
 	}
 
-	function examine(str, foundTags) {
-		foundTags[str] = true;
+	function iterateObject(obj, callback) {
+		var name;
+
+		for (name in obj) {
+			if (obj.hasOwnProperty(name)) {
+				callback(obj[name], name, obj);
+			}
+		}
 	}
 
-	function scan(domnode, foundTags) {
+	/**
+	 * Scanning the DOM for tags
+	 */
+
+	proto.examine = function (str) {
+		str = str.replace(/:/g, '-');  // ng:app => ng-app
+		str = str.replace(/^(data|x)-/, '');  // x-ng-app, data-ng-app => ng-app
+		this.foundTags[str] = true;
+	};
+
+	proto.scan = function (domnode) {
+		var myself;
+
 		function addAttrs(attrib) {
-			examine(attrib.name, foundTags);
+			myself.examine(attrib.name);
 		}
 
+		myself = this;
+
 		while (domnode) {
-			examine(domnode.nodeName.toLowerCase(), foundTags);
+			this.examine(domnode.nodeName.toLowerCase());
 			iterate(domnode.attributes, addAttrs);
 
 			if (domnode.hasChildNodes(domnode)) {
-				scan(domnode.firstChild, foundTags);
+				this.scan(domnode.firstChild);
 			}
 
 			domnode = domnode.nextSibling;
 		}
-	}
+	};
 
-	function resolveDependencies(moduleName, rules, neededModules) {
-		if (neededModules[moduleName]) {
+	/**
+	 * Finding what modules to load
+	 */
+	proto.resolveDependencies = function (moduleName) {
+		var myself;
+
+		if (this.neededModules[moduleName]) {
 			return;
 		}
 
-		if (!rules[moduleName]) {
+		if (!this.rules[moduleName]) {
 			return;
 		}
 
-		// Put dependencies in first
-		iterate(rules[moduleName].depends, function (dependsOn) {
-			resolveDependencies(dependsOn, rules, neededModules);
+		this.neededModules[moduleName] = true;
+		myself = this;
+		iterate(this.rules[moduleName].depends, function (dependsOn) {
+			myself.resolveDependencies(dependsOn);
 		});
+	};
 
-		neededModules[moduleName] = true;
-	}
+	proto.detectModules = function () {
+		var moduleName, myself;
 
-	function detectModules(rules, foundTags) {
-		var moduleName, neededModules = {};
+		myself = this;
 
-		function testModule(attribName) {
-			if (foundTags[attribName]) {
-				resolveDependencies(moduleName, rules, neededModules);
+		iterateObject(this.rules, function (moduleRules, moduleName) {
+			iterate(moduleRules.detect, function (attribName) {
+				if (myself.foundTags[attribName]) {
+					myself.resolveDependencies(moduleName);
+				}
+			});
+		});
+	};
+
+	/**
+	 * Add nodes for scripts and css
+	 */
+
+	proto.addHeadNode = function (node) {
+		document.getElementsByTagName('head')[0].appendChild(node);
+	};
+
+	proto.addNodeOnload = function (node, callback) {
+		function nodeLoaded(evt) {
+			var node = evt.currentTarget || evt.srcElement;
+
+			if (evt.type === 'load' || node.readyState === 'complete' || node.readyState === 'loaded') {
+				callback();
 			}
 		}
 
-		for (moduleName in rules) {
-			if (rules.hasOwnProperty(moduleName)) {
-				iterate(rules[moduleName].detect, testModule);
-			}
+		if (node.addEventListener) {
+			node.addEventListener("load", nodeLoaded, false);
+		} else {
+			node.attachEVent("onreadystatechange", nodeLoaded);
 		}
+	};
 
-		return neededModules;
-	}
-
-	function addScriptTag(url) {
+	proto.addScriptTag = function (url) {
 		var node;
 		node = document.createElement('script');
 		node.type = 'text/javascript';
 		node.charset = 'utf-8';
 		node.src = url;
-		document.head.appendChild(node);
-	}
+		this.addHeadNode(node);
+		return node;
+	};
 
-	function addLinkTag(url) {
+	proto.addLinkTag = function (url) {
 		var node;
 		node = document.createElement('link');
 		node.rel = 'stylesheet';
 		node.type = 'text/css';
 		node.href = url;
-		document.head.appendChild(node);
-	}
+		this.addHeadNode(node);
+		return node;
+	};
 
-	function addTags(needed, rules) {
-		var moduleName;
+	proto.addTags = function (moduleName, onload) {
+		var myself;
+		myself = this;
+		console.log(this.rules, moduleName);
+		iterate(this.rules[moduleName].js, function (url) {
+			var node;
+			node = myself.addScriptTag(url);
 
-		for (moduleName in needed) {
-			if (needed.hasOwnProperty(moduleName)) {
-				iterate(rules[moduleName].js, addScriptTag);
-				iterate(rules[moduleName].css, addLinkTag);
+			if (onload) {
+				myself.addNodeOnload(node, onload);
 			}
+		});
+		iterate(this.rules[moduleName].css, function (url) {
+			myself.addLinkTag(url);
+		});
+	};
+
+	/**
+	 * Determine what to load and what to do when done loading
+	 */
+
+
+	proto.loadModule = function (moduleName) {
+		var myself;
+		myself = this;
+		this.numberLoading += 1;
+		this.addTags(moduleName, function () {
+			myself.numberLoading -= 1;
+			myself.loadedModules[moduleName] = true;
+			myself.loadModules();
+		});
+		iterate(this.rules[moduleName].module, function (angularModule) {
+			myself.angularModules.push(angularModule);
+		});
+	};
+
+	proto.loadModules = function () {
+		var modulesSkipped, myself;
+
+		function hasUnmetDepends(moduleName) {
+			var misses = 0;
+			iterate(myself.rules[moduleName].depends, function (needed) {
+				if (!myself.loadedModules[needed]) {
+					misses += 1;
+				}
+			});
+			return misses;
 		}
-	}
 
-	// Detecting load event, thanks to jQuery
-	function loaded() {
-		var needed, foundTags;
-		document.removeEventListener("DOMContentLoaded", loaded, false);
-		window.removeEventListener("load", loaded, false);
-		foundTags = {};
-		scan(document, foundTags);
-		needed = detectModules(window.angularAutoload, foundTags);
-		addTags(needed, window.angularAutoload);
-		setTimeout(function () {
-			console.log('bootstrap');
-			angular.bootstrap(document, ['autoGrow']);
-		}, 1000);
-	}
+		modulesSkipped = 0;
+		myself = this;
+		iterateObject(this.neededModules, function (bool, moduleName) {
+			if (myself.loadedModules[moduleName]) {
+				return;
+			}
 
-	document.addEventListener("DOMContentLoaded", loaded, false);
-	window.addEventListener("load", loaded, false);
+			if (hasUnmetDepends(moduleName)) {
+				modulesSkipped += 1;
+				return;
+			}
+
+			myself.loadModule(moduleName);
+		});
+
+		if (!this.numberLoading && !modulesSkipped) {
+			this.loadingComplete();
+		}
+	};
+
+	proto.loadingComplete = function () {
+		// All done
+		if (this.loadedModules.angular) {
+			angular.bootstrap(document, this.angularModules);
+		}
+	};
+
+	/**
+	 * Initialization code - attach to dom loaded
+	 */
+	proto.init = function () {
+		var myself;
+
+		// Detecting load event, thanks to jQuery
+		function loaded() {
+			document.removeEventListener("DOMContentLoaded", loaded, false);
+			window.removeEventListener("load", loaded, false);
+			myself.scan(document);  // Scan document for all potential module names
+			myself.detectModules();  // Detect any modules we need and dependencies
+			myself.loadModules();  // Keep calling this until we are loaded
+		}
+
+		myself = this;
+		document.addEventListener("DOMContentLoaded", loaded, false);
+		window.addEventListener("load", loaded, false);
+	};
+
+	window.AngularAutoloader = AngularAutoloader;
 }());
