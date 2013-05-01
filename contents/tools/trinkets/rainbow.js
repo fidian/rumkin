e066cfd20cd6b6042fb838991c9dd2c282fefaa0
Tyler Akins <fidian@rumkin.com>
2013-05-01 16:20:23 -0500

    Rework rainbow into a directive

---
+++ contents/tools/trinkets/rainbow.js
@@ -0,0 +1,50 @@
+/* Rainbow text generator
+ * Copyright 2012 Tyler Akins
+ * http://rumkin.com/license.html
+ */
+/*global angular, autoloader, util*/
+(function () {
+	'use strict';
+
+	function makeHtml(input) {
+		var output;
+
+		function colorHex(i, mult) {
+			var pi, sv, dec;
+			pi = 3.141592653;
+			sv = i / (input.length / pi);
+			sv += mult * (pi / 3);
+			dec = Math.sin(sv);
+			return util.hexByte(dec * dec * 255);
+		}
+
+		output = input.split('').map(function (c, i) {
+			var color;
+
+			if (c === ' ') {
+				return c;
+			}
+
+			color = "#";
+			color += colorHex(i, 1);
+			color += colorHex(i, 0);
+			color += colorHex(i, -1);
+			return '<span style="color:' + color + '">' + util.htmlencode(c) + '</span>';
+		}).join('');
+
+		return output;
+	}
+
+	angular.module('rainbow', ['autoGrow']).directive("rainbow", function () {
+		return {
+			link: function ($scope, element, attrs) {
+				$scope.input = '';
+				$scope.output = '';
+
+				$scope.$watch('input', function (newVal) {
+					$scope.output = makeHtml(newVal);
+				});
+			}
+		};
+	});
+}());
--- contents/tools/trinkets/rainbow.md
+++ contents/tools/trinkets/rainbow.md
@@ -1,19 +1,22 @@
 ---
 title: Rainbow Text Generator
 template: page.jade
-js: /js/util.js /js/angular.min.js /js/directives/auto-grow.js rainbow-controller.js
-controller: RainbowController
+js: /js/util.js /js/angular.min.js /js/directives/auto-grow.js rainbow.js
 module: rainbow
 ---
 
 Enter in anything that you want converted into a rainbow.  It will gently fade from letter to letter.  Then, you can copy the generated HTML into your web page.
 
-<input type="text" ng-model="input" class="wide">
+<div rainbow>
+	<input type="text" ng-model="input" class="wide">
 
-This is the result:
+	<p>This is the result:</p>
 
-<p class="outline"><b ng-bind-html-unsafe="output"></b></p>
+	<p class="outline"><b ng-bind-html-unsafe="output"></b></p>
 
-And here's the HTML in case you like what you see:
+	<p>And here's the HTML in case you like what you see:</p>
 
-<div class="outline" ng-bind="output"></div>
+	<code class="wrap"><small ng-bind="output"></small></code>
+</div>
+
+
