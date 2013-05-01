2b33c931b345bc2945f35c8197fbaafdf97cfd7b
Tyler Akins <fidian@rumkin.com>
2013-05-01 16:44:35 -0500

    Moved to a directive

---
+++ contents/tools/trinkets/base-n.js
@@ -0,0 +1,62 @@
+/*global angular, autoloader*/
+(function () {
+	'use strict';
+
+	var numList;
+
+	numList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	function recalc(inBase, outBase, number) {
+		var convertedNumber, i, idx, s;
+
+		inBase = +inBase;
+		outBase = +outBase;
+		number = number.toUpperCase();
+		convertedNumber = 0;
+		s = '';
+
+		// Convert the input number
+		for (i = 0; i < number.length; i += 1) {
+			idx = numList.indexOf(number.charAt(i));
+
+			if (idx >= 0) {
+				convertedNumber *= inBase;
+				convertedNumber += idx;
+			}
+		}
+
+		// Convert to the output number base
+		while (convertedNumber) {
+			idx = convertedNumber % outBase;
+			s = numList.charAt(idx) + s;
+			convertedNumber -= idx;
+			convertedNumber /= outBase;
+		}
+
+		// Handle zero, the only special case
+		if (!s.length) {
+			s = '0';
+		}
+
+		return s;
+	}
+
+	angular.module('baseN', []).directive("baseN", function () {
+		return {
+			link: function ($scope, element, attrs) {
+				function update() {
+					$scope.output = recalc($scope.inbase, $scope.outbase, $scope.input);
+				}
+
+				$scope.inbase = 10;
+				$scope.outbase = 10;
+				$scope.input = '';
+				$scope.output = '';
+				$scope.$watch('inbase', update);
+				$scope.$watch('outbase', update);
+				$scope.$watch('input', update);
+			}
+		};
+	});
+}());
+
