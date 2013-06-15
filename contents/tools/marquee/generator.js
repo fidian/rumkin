c348f2812b10714c99689998f9052c0c877ea744
Tyler Akins <fidian@rumkin.com>
2013-06-14 21:16:05 -0500

    Making an Angular form

--- contents/tools/marquee/generator.js
+++ contents/tools/marquee/generator.js
@@ -7,7 +7,7 @@
 (function () {
 	'use strict';
 
-	var hideMethods, showMethods;
+	var module;
 
 	window.generator = {
 		depends: {},
@@ -15,9 +15,95 @@
 		hide: {}
 	};
 
-	angular.module('generator', []).directive("generator", function () {
+	module = angular.module('generator', []);
+
+	module.directive("generator", function () {
 		return {
-			link: function ($scope, element, attrs) {
+			link: function (scope, element, attrs) {
+				function updatePreview() {
+					var preview;
+
+					preview = {
+						message: scope.message,
+						show: scope.showMethod,
+						readDelay: scope.readDelay,
+						hide: scope.hideMethod,
+						betweenDelay: scope.betweenDelay
+					};
+
+					if (!preview.show) {
+						preview.show = scope.showMethodList.none;
+					}
+
+					if (!preview.hide) {
+						preview.hide = scope.showMethodList.none;
+					}
+
+					scope.preview = [
+						preview
+					];
+				}
+
+				scope.animationList = [];
+				scope.depends = window.generator.depends;
+				scope.hideMethodList = window.generator.hide;
+				scope.showMethodList = window.generator.show;
+				scope.$watch('betweenDelay', updatePreview);
+				scope.$watch('readDelay', updatePreview);
+				scope.$watch('message', updatePreview);
+				scope.setHideMethod = function (method) {
+					scope.hideMethod = method;
+					updatePreview();
+				};
+				scope.setShowMethod = function (method) {
+					scope.showMethod = method;
+					updatePreview();
+				};
+
+			}
+		};
+	});
+
+	module.directive('generatorMethod', function () {
+		return {
+			link: function (scope, element, attrs) {
+				scope.$watch('method', function (newVal) {
+					scope.callback({
+						method: newVal
+					});
+
+					// Reset current values for variables
+					if (newVal && newVal.variables) {
+						newVal.variables.forEach(function (variable) {
+							variable.currentValue = variable['default'];
+						});
+					}
+				});
+			},
+			scope: {
+				callback: '&callback',
+				label: '=label',
+				methodList: '=generatorMethod'
+			},
+			templateUrl: 'method'
+		};
+	});
+
+	module.directive('generatorDemo', function () {
+		var i;
+		i = 0;
+		return {
+			link: function (scope, element, attrs) {
+				element.val(i);
+				i += 1;
+				scope.$watch('animations', function () {
+					console.log(scope.animations);
+					element.val(i);
+					i += 1;
+				});
+			},
+			scope: {
+				animations: '=generatorDemo'
 			}
 		};
 	});
