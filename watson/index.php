78e0c52e7cc6b5c6191ae42a7610b3ae52a67edc
Tyler Akins <fidian@rumkin.com>
2013-07-05 09:50:32 -0500

    Adding $_SERVER to the output

--- watson/index.php
+++ watson/index.php
@@ -8,6 +8,7 @@ $sections['Cookies'] = Dump::out($request->cookie())->returned();
 $sections['GET Variables'] = Dump::out($request->get())->returned();
 $sections['POST Variables'] = Dump::out($request->post())->returned();
 $sections['Request Headers'] = Dump::out(apache_request_headers())->returned();
+$sections['SERVER Properties'] = Dump::out($_SERVER)->returned();
 
 $ul = new Ultralite(__DIR__ . '/templates');
 $ul->sections = $sections;
