11b80785729d9b320ada31ee285afbf7319446e9
Tyler Akins <fidian@rumkin.com>
2015-04-23 19:46:15 -0500

    Adding map file to eliminate errors in Chrome console

---
+++ site/tools/decision-tree/decision-tree.js
@@ -0,0 +1,90 @@
+/*global angular*/
+
+angular.module('decisionTree', [
+    'hc.marked'
+]);
+
+angular.module('decisionTree').config([
+    '$locationProvider',
+    function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
+]);
+
+angular.module('decisionTree').controller('decisionTreeController', [
+    '$scope',
+    '$location',
+    '$http',
+    '$q',
+    function ($scope, $location, $http, $q) {
+        function loadTree(name, previous) {
+            var deferred, tree;
+
+            deferred = $q.defer();
+
+            // Sanitize
+            name = name.replace(/[^\-a-z0-9]/g, '');
+
+            if (previous && previous.name === name) {
+                deferred.resolve(previous);
+            } else {
+                tree = {
+                    name: name,
+                    isLoading: true,
+                    title: ''
+                };
+
+                $http.get(name + '.js').then(function (response) {
+                    tree.isLoading = false;
+                    tree.isLoaded = true;
+                    tree.data = response.data;
+                    tree.title = tree.data.title;
+                    deferred.resolve(tree);
+                }, function (err) {
+                    tree.isLoading = false;
+                    tree.isError = true;
+                    tree.error = err.data;
+                    tree.title = 'Error loading tree';
+                    deferred.resolve(tree);
+                });
+            }
+
+            return deferred.promise;
+        }
+
+        function loadQuestion(tree, id) {
+            if (! id || ! tree.data.tree[id]) {
+                id = tree.data.start;
+            }
+
+            return tree.data.tree[id];
+        }
+
+        function reconfigure() {
+            var name;
+
+            name = $location.search().tree;
+
+            if (name) {
+                loadTree(name, $scope.tree).then(function (tree) {
+                    $scope.tree = tree;
+                    $scope.question = loadQuestion($scope.tree, $location.search().q);
+                });
+            } else {
+                $scope.tree = null;
+                $scope.question = null;
+            }
+        }
+
+        $scope.showTree = function (name) {
+            $location.search('tree', name);
+        };
+
+        $scope.selectAnswer = function (answer) {
+            $location.search('q', answer);
+        };
+
+        reconfigure();
+        $scope.$on('$locationChangeSuccess', reconfigure);
+    }
+]);
---
+++ site/tools/decision-tree/diablo-ii.js
@@ -0,0 +1,99 @@
+{
+    "start": "Start",
+    "title": "Diablo II Problem Solver",
+    "tree": {
+        "Start": {
+            "text": "This series of questions has been designed to assist you in determining what is wrong with your installation of Diablo II.  By clicking on the appropriate answer, you will navigate to a page that will (in most cases) solve the issue at hand.\n\nThe first question is pretty easy; do you use *any* hacks with Diablo II?  (This includes C3PO, a map hack, D2Loader, D2JSP, CD Key Refiller, and many other things.)",
+            "answers": {
+                "UsingHacks": "Yes",
+                "NoHacks": "No"
+            }
+        },
+        "NoHacks": {
+            "text": "Good for you!  There are very few people out there who choose to be handicapped by a game.  No matter what your problem is, you should reinstall Diablo II",
+            "answers": {
+                "ReinstallDiabloII": "Ok, let's do it!"
+            }
+        },
+        "UsingHacks": {
+		    "text": "Are you experiencing a specific error message that appears outside of the Diablo II game?  Some sort of pop-up error box?  This only refers to errors that pop up when you intend to play Diablo II; not when you run external hacks (like the CD Key Changer).",
+            "answers": {
+                "SeeErrorMessage": "Yes",
+                "NoErrorMessage": "No"
+            }
+        },
+        "SeeErrorMessage": {
+            "text": "Which error message are you seeing?",
+            "answers": {
+                "MissingStormDll": "Missing Storm.Dll",
+                "BigErrorHere": "We've Got A Big Error Here",
+                "UnhandledException": "Unhandled Exception",
+                "OtherError": "Other ..."
+            }
+        },
+        "NoErrorMessage": {
+            "text": "So, you use hacks and you don't have a specific pop-up error message.  Sorry, but I can not help you diagnose this problem."
+        },
+        "MissingStormDll": {
+		    "text": "The \"Missing Storm.DLL\" error only appears when you are trying to run D2Loader and it isn't installed correctly.\n\nAre you certain that you installed D2Loader to your main Diablo II directory?  (If you installed it with the Hack Pack, it was installed to the correct location.)",
+            "answers": {
+                "InstalleldD2LoaderCorrectly": "Yes, I am certain",
+                "TryInstallinlgD2LoaderInRightSpot": "No, I am not completely positive"
+            }
+        },
+        "BigErrorHere": {
+            "text": "The \"big error\" dialog box happens when a plugin crashes.  If it just happens when you leave the game, try to cope.\n\nDo you use C3PO?",
+            "answers": {
+                "UsingC3PO": "Yes I do",
+                "PluginConflict": "No I do not"
+            }
+        },
+        "UnhandledException": {
+            "text": "If you get any \"unhandled exception\" errors, a wide variety of things could be wrong.  This is a list of the most common reasons for this problem.\n\n* Software corruption (Could be a plugin, Diablo II, or even Windows; try reinstalling them in that order)\n* Faulty/bad memory\n* Error caused by CPU malfunction (Most often caused by overclocking too high and not cooling your processor enough)"
+        },
+        "OtherError": {
+            "text": "I do not have an extensive list of error messages and this game is too old for me to keep diagnosing.  Sorry."
+        },
+        "InstalledD2LoaderCorrectly": {
+            "text": "Run D2Loader directly from your Diablo II folder (My Computer → C: → Program Files → Diablo II).  If you installed it by hand and followed the instructions, it was installed as `Diablo II.exe`.  If you used the Hack Pack or just copied D2Loader in, it will be called `Diablo II Loader.exe`.  Double-click on the appropriate file to run it.\n\nDoes the message come up again saying that `storm.dll` is missing?",
+            "answers": {
+                "StillMissingStormDll": "Yes it does",
+                "ShortcutProblemStormDll": "No it does not"
+            }
+        },
+        "TryInstallingD2LoaderInRightSpot": {
+            "text": "Reinstall D2Loader, making sure that it is placed in your Diablo II folder.  You will likely be installing this by hand, so move the files to your Diablo II directory before you run them. If you have any problems or don't want to install it yourself, just download the Hack Pack if you can find it (it is an easy to use installer that lets you decide what hacks you want to run)."
+        },
+        "UsingC3PO": {
+            "text": "You likely have a plugin conflict with C3PO.  Try adding `-noplugin` to your command line options to D2Loader.  Does that work?",
+            "answers": {
+                "C3POPluginConflict": "Yes it works with `-noplugin`",
+                "BigErrorWithNoPlugins": "No, it still fails"
+            }
+        },
+        "PluginConflict": {
+            "text": "Try removing all of your plugins.  Add them in, one at a time until you determine which one is causing problems.  It could be a combination of plugins.  For instance, Mousepad's maphack works great by itself.  Antidetection works great by itself.  If you try to load them both at once, you will experience problems."
+        },
+        "StillMissingStormDll": {
+            "text": "Likely, the `storm.dll` file was corrupted or deleted from your hard drive.  Looks like you need to reinstall Diablo II.",
+            "answers": {
+                "ReinstallDiabloII": "Bummer.  Ok, how do I do that?"
+            }
+        },
+        "ShortcutProblemStormDll": {
+            "text": "The problem is not with D2Loader, but more likely with the shortcut that runs D2Loader.  Right-click on your shortcut and select Properties.  Look for a \"Start In\" property that should say `C:\\Program Files\\Diablo II`.  If it does not say the path to your Diablo II installation, it must be changed."
+        },
+        "C3POPluginConflict": {
+            "text": "You likely have a plugin conflict.  Two plugins could be messing each other up, or a single plugin may be conflicting with C3PO.  For instance, since C3PO contains a maphack and its own antidetection, you are not allowed to load another Antidetection plugin or another maphack.\n\nRemove all plugins and make sure Diablo II runs.  Then add plugins back in, one at a time.  Figure out which plugins conflict through trial and error."
+        },
+        "BigErrorWithNoPlugins": {
+            "text": "Most likely C3PO, a plugin, D2Loader, or something else got corrupted.  I would strongly suggest reinstalling Diablo II.",
+            "answers": {
+                "ReinstallDiabloII": "Where are the instructions to reinstall Diablo II?"
+            }
+        },
+        "ReinstallDiabloII": {
+            "text": "Reinstalling Diablo II isn't too hard.\n\n1. Uninstall Diablo II (Start → Settings → Control Panel → Add/Remove Programs → Diablo II)\n2. Delete your Diablo II folder (My Computer → C: → Program Files → Diablo II then right click and select Delete)\n3. Reinstall Diablo II (insert installation CD and follow instructions)\n4. [Optional] Reinstall expansion pack\n5. Connect to Battle.Net and upgrade to the latest version\n6. [Optional] Reinstall your hacks"
+        }
+    }
+}
