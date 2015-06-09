d544db8f844ba4cef6c8686592dc1606cc62213b
Tyler Akins <fidian@rumkin.com>
2015-06-09 09:31:29 -0500

    New module that makes accessing search parameters trivial

---
+++ site/js/modules/search-scope.js
@@ -0,0 +1,22 @@
+/*global angular*/
+angular.module('searchScope', []).config([
+    '$locationProvider',
+    function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
+]).controller('searchScope', [
+    '$scope',
+    '$location',
+    function ($scope, $location) {
+        $scope.search = $location.search();
+    }
+]).directive('searchScope', [
+    '$location',
+    function ($location) {
+        return {
+            link: function ($scope) {
+                $scope.search = $location.search();
+            }
+        };
+    }
+]);
