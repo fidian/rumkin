d0789591703137aae19c66912585d8fdd2f6c90d
Tyler Akins <fidian@rumkin.com>
2015-08-03 16:14:18 -0500

    Update for newer Angular

--- site/js/modules/auto-grow.js
+++ site/js/modules/auto-grow.js
@@ -13,53 +13,69 @@
  * Usage: <textarea auto-grow></textarea>
  */
 /*global angular, document*/
-(function () {
-	'use strict';
-	angular.module('autoGrow', []).directive('autoGrow', function () {
-		return function (scope, element, attr) {
-			var minHeight = element[0].offsetHeight,
-				paddingLeft = element.css('paddingLeft'),
-				paddingRight = element.css('paddingRight'),
-				$shadow = angular.element('<div></div>').css({
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: element[0].offsetWidth - parseInt(paddingLeft || 0, 10) - parseInt(paddingRight || 0, 10),
-					fontSize: element.css('fontSize'),
-					fontFamily: element.css('fontFamily'),
-					lineHeight: element.css('lineHeight'),
-					resize:     'none',
-					visibility: 'hidden'
-				}),
-				update = function () {
-					function times(string, number) {
-						var i, r;
-						for (i = 0, r = ''; i < number; i += 1) {
-							r += string;
-						}
-						return r;
-					}
+angular.module('autoGrow', []).directive('autoGrow', [
+    '$timeout',
+    function ($timeout) {
+        'use strict';
+        return function ($scope, element, attr) {
+            var css, minHeight, paddingLeft, paddingRight, shadow;
 
-					var val = element.val().replace(/</g, '&lt;')
-						.replace(/>/g, '&gt;')
-						.replace(/&/g, '&amp;')
-						.replace(/\n$/, '<br/>&nbsp;')
-						.replace(/\n/g, '<br/>')
-						.replace(/\s{2,}/g, function (space) {
-							return times('&nbsp;', space.length - 1) + ' ';
-						});
-					$shadow.html("<br/>" + val);
-					element.css('height', Math.max($shadow[0].offsetHeight, minHeight) + 'px');
-				};
+            minHeight = element[0].offsetHeight;
+            paddingLeft = parseInt(element.css('paddingLeft') || 0, 10);
+            paddingRight = parseInt(element.css('paddingRight') || 0, 10);
+            css = {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: element[0].offsetWidth - paddingLeft - paddingRight + 'px',
+                fontSize: element.css('fontSize'),
+                fontFamily: element.css('fontFamily'),
+                lineHeight: element.css('lineHeight'),
+                resize:     'none',
+                visibility: 'hidden'
+            };
+            shadow = angular.element('<div></div>');
+            $scope.$watch(attr.ngModel, function (newValue) {
+                var val;
 
-			angular.element(document.body).append($shadow);
+                function times(string, number) {
+                    var i, r;
 
-			scope.$on('$destroy', function () {
-				$shadow.remove();
-			});
-			element.bind('keyup keydown keypress change', update);
-			update();
-			element.addClass('auto-grow');
-		};
-	});
-}());
+                    for (i = 0, r = ''; i < number; i += 1) {
+                        r += string;
+                    }
+
+                    return r;
+                }
+
+                if (! newValue) {
+                    newValue = "";
+                }
+
+                if (! shadow) {
+                    return;
+                }
+
+                val = newValue.replace(/</g, '&lt;')
+                    .replace(/>/g, '&gt;')
+                    .replace(/&/g, '&amp;')
+                    .replace(/\n$/, '<br/>&nbsp;')
+                    .replace(/\n/g, '<br/>')
+                    .replace(/\s{2,}/g, function (space) {
+                        return times('&nbsp;', space.length - 1) + ' ';
+                    });
+                shadow.html("<br/>" + val);
+                $timeout(function () {
+                    element.css('height', Math.max(shadow[0].offsetHeight, minHeight) + 'px');
+                }, 1);
+            });
+            angular.element(document.body).append(shadow);
+            shadow.css(css);
+
+            $scope.$on('$destroy', function () {
+                shadow.remove();
+            });
+            element.addClass('auto-grow');
+        };
+    }
+]);
