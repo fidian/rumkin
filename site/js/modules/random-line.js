830bc540f05d9930830977d4ea037243c2d17ca7
Tyler Akins <fidian@rumkin.com>
2017-04-17 12:32:33 -0500

    Linking differently and updating

--- site/js/modules/random-line.js
+++ site/js/modules/random-line.js
@@ -9,25 +9,27 @@
  * @see https://rumkin.com/license/
  */
 /* global angular */
-angular.module("randomLine", []).directive("randomLine", [
-    "$http",
-    function ($http) {
-        return {
-            link($scope, $element, $attrs) {
-                $element.text("");
+angular.module("randomLine", []).directive("randomLine", ($http) => {
+    return {
+        link($scope) {
+            console.log($scope);
+            $scope.line = "Error with URI.";
+            $http.get($scope.uri, {
+                cache: true
+            }).then((response) => {
+                var index, list;
 
-                $http.get($attrs.randomLine, {
-                    cache: true
-                }).then((response) => {
-                    var index, list;
-
-                    list = response.data.replace(/^[ \t\r\n]*|[ \t\r\n]$/g, "").split(/[\r\n]+/);
-                    index = Math.floor(list.length * Math.random());
-                    $element.text(list[index]);
-                }, () => {
-                    $element.text("Unable to retrieve list of lines");
-                });
-            }
-        };
-    }
-]);
+                list = response.data.replace(/^[ \t\r\n]*|[ \t\r\n]$/g, "").split(/[\r\n]+/);
+                index = Math.floor(list.length * Math.random());
+                $scope.line = list[index];
+            }, () => {
+                $scope.line = "Unable to retrieve list.";
+            });
+            $scope.line = "";
+        },
+        scope: {
+            uri: "=randomLine"
+        },
+        template: "{{line}}"
+    };
+});
