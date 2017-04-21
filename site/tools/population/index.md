835e40bdd5cb68f7cd536be484364032e1131683
Tyler Akins <fidian@rumkin.com>
2017-04-21 14:54:22 -0500

    Population updates - now works

---
+++ site/tools/population/index.md
@@ -0,0 +1,83 @@
+---
+title: Population Counter
+js: population.js
+controller: populationController
+module: population
+---
+
+<div ng-if="!loaded">
+    <p>
+        Loading the population statistics.
+    </p>
+</div>
+<div ng-if="loaded && !selected">
+    <p>
+        Select a country or region in order to see its estimated population.
+    </p>
+    <h2>
+        Regions
+    </h2>
+    <ul>
+        <li ng-repeat="region in regions">
+            <a ng-click="selectCountry(region.I)" ng-bind="region.L" href="#"></a> (<span ng-bind="region.population | number"></span>)
+        </li>
+    </ul>
+    <h2>
+        Countries
+    </h2>
+    <ul>
+        <li ng-repeat="country in countries">
+            <a ng-click="selectCountry(country.I)" ng-bind="country.L" href="#"></a> (<span ng-bind="country.population | number"></span>)
+        </li>
+    </ul>
+</div>
+<div ng-if="loaded && selected">
+    <h2 ng-bind="selected.L"></h2>
+    <p>
+        Current estimated population: <span ng-bind="selected.population | number"></span>
+    </p>
+    <p>
+        All projections start from <span ng-bind="statsStartDate | date:'longDate':'UTC'"></span>. It's been <span ng-bind="selected.days | number"></span> days since.
+    </p>
+    <table>
+        <tr>
+            <th>Births</th>
+            <td ng-bind="selected.births | number"></td>
+        </tr>
+        <tr>
+            <th>Last Birth</th>
+            <td>
+                <span ng-if="selected.birthNote.male">Male</span><span ng-if="!selected.birthNote.male">Female</span>, may live to be <span ng-bind="selected.birthNote.ageRange"></span>
+            </td>
+        </tr>
+        <tr>
+            <th>Deaths</th>
+            <td ng-bind="selected.deaths | number"></td>
+        </tr>
+        <tr>
+            <th>Last Death</th>
+            <td>
+                <span ng-if="selected.deathNote.male">Male</span><span ng-if="!selected.deathNote.male">Female</span>, age could have been <span ng-bind="selected.deathNote.ageRange"></span>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                Migrations <span ng-if="selected.migrations > 0">In</span><span ng-if="selected.migrations < 0">Out</span>
+            </th>
+            <td>
+                <span ng-if="selected.migrations > 0" ng-bind="selected.migrations | number"></span>
+                <span ng-if="selected.migrations < 0" ng-bind="- selected.migrations | number"></span>
+                <span ng-if="!selected.migrations">None</span>
+            </td>
+        </tr>
+    </table>
+    <p>
+        <a href="#" ng-click="selectCountry()">← Back to the country list.</a>
+    </p>
+</div>
+
+The figures presented here are based on information from the [United Nations](https://esa.un.org/unpd/wpp/Download/Standard/ASCII/). These are estimates. While this is fun to watch and is roughly accurate, the numbers do not reflect the real population. This performs a straight-line estimate for populations, assuming the rates do not change.
+
+Numbers and notes will automatically update with new information whenever a new simulated birth, death or migration happens. The information about the person's age is randomly generated based on percentages of actual deaths in the country, but certainly does not represent a real person.
+
+Estimates use your computer's time, so use the right time or you'll have very inaccurate results.
---
+++ site/tools/population/population.js
@@ -0,0 +1,232 @@
+"use strict";
+
+/* global angular */
+angular.module("population", []);
+
+angular.module("population").config(($locationProvider) => {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
+
+angular.module("population").controller("populationController", ($http, $interval, $location, $scope) => {
+    /**
+     * Keep some of the the response data, possibly remap the name, then
+     * sort by the name.
+     *
+     * @param {Object} data
+     * @param {Object} rename
+     * @param {Function} filterFn
+     * @return {Array.<Object>}
+     */
+    function filterAreas(data, rename, filterFn) {
+        var countries;
+
+        countries = [];
+        Object.keys(data).forEach((key) => {
+            var value;
+
+            value = data[key];
+
+            if (filterFn(value)) {
+                countries.push(value);
+            }
+
+            if (rename[value.I]) {
+                value.L = rename[value.I];
+            }
+        });
+
+        countries.sort((a, b) => {
+            var aVal, bVal;
+
+            aVal = a.L.toLowerCase();
+            bVal = b.L.toLowerCase();
+
+            if (aVal < bVal) {
+                return -1;
+            }
+
+            if (aVal > bVal) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+        return countries;
+    }
+
+
+    /**
+     * Assigns a particular country to the view.
+     */
+    function assignCountry() {
+        var countryId;
+
+        countryId = $location.search().selected;
+
+        if (!countryId) {
+            $scope.selected = null;
+        } else if ($scope.statsById[countryId]) {
+            $scope.selected = $scope.statsById[countryId];
+        } else {
+            $scope.selected = null;
+            $location.search("selected", null);
+        }
+    }
+
+
+    /**
+     * Update the location to choose a country.
+     *
+     * @param {string} countryId
+     */
+    function selectCountry(countryId) {
+        $location.search("selected", countryId || null);
+    }
+
+
+    /**
+     * Find an age in the age ranges. If none are found due to rounding
+     * errors or invalid data, this returns the last age range.
+     *
+     * @param {Object} ages
+     * @return {string}
+     */
+    function findAge(ages) {
+        var i, key, keys, r;
+
+        r = Math.random();
+        key = "?";
+        keys = Object.keys(ages);
+
+        for (i = 0; i < keys.length; i += 1) {
+            key = keys[i];
+
+            if (r < ages[key]) {
+                return key;
+            }
+
+            r -= ages[key];
+        }
+
+        return key;
+    }
+
+
+    /**
+     * Creates a note for a birth or death.
+     *
+     * @param {number} maleProbability
+     * @param {Object} maleAges
+     * @param {Object} femaleAges
+     * @return {Object}
+     */
+    function makeNote(maleProbability, maleAges, femaleAges) {
+        var ageRange, male;
+
+        male = Math.random() <= maleProbability;
+
+        if (male) {
+            ageRange = findAge(maleAges);
+        } else {
+            ageRange = findAge(femaleAges);
+        }
+
+        return {
+            ageRange,
+            male
+        };
+    }
+
+
+    /**
+     * Update the population numbers for a country or region.
+     *
+     * @param {Object} stats
+     */
+    function updatePopulation(stats) {
+        var births, deaths, now, years;
+
+        now = new Date();
+        stats.days = (now - $scope.statsStartDate) / 86400000;
+        years = stats.days / 365.2425;
+        births = Math.floor(stats.B * years);
+
+        if (stats.births !== births) {
+            stats.births = births;
+            stats.birthNote = makeNote(stats.BMP, stats.DM, stats.DF);
+        }
+
+        deaths = Math.floor(stats.D * years);
+
+        if (stats.deaths !== deaths) {
+            stats.deaths = deaths;
+            stats.deathNote = makeNote(stats.DMP, stats.DM, stats.DF);
+        }
+
+        stats.migrations = Math.floor(stats.M * years);
+        stats.population = stats.P + stats.births - stats.deaths + stats.migrations;
+    }
+
+
+    $scope.loaded = false;
+    $scope.selectCountry = selectCountry;
+    $scope.statsStartDate = new Date("2013-01-01T00:00:00Z");
+    $http.get("populations.json").then((response) => {
+        $scope.loaded = true;
+        $scope.statsById = response.data;
+        $scope.countries = filterAreas(response.data, {}, (x) => {
+            return x.I < 900;
+        });
+        $scope.regions = filterAreas(response.data, {
+            903: "Africa",
+            910: "Africa: Eastern",
+            911: "Africa: Middle",
+            912: "Africa: Northern",
+            913: "Africa: Southern",
+            947: "Africa: Sub-Saharan",
+            914: "Africa: Western",
+            935: "Asia",
+            5500: "Asia: Central",
+            906: "Asia: Eastern",
+            921: "Asia: South-Central",
+            920: "Asia: South-Eastern",
+            5501: "Asia: Southern",
+            922: "Asia: Western",
+            908: "Europe",
+            923: "Europe: Eastern",
+            924: "Europe: Northern",
+            925: "Europe: Southern",
+            926: "Europe: Western",
+            904: "Latin America and the Caribbean",
+            915: "Latin America and the Caribbean: Caribbean",
+            916: "Latin America and the Caribbean: Central America",
+            931: "Latin America and the Caribbean: South America",
+            905: "Northern America",
+            909: "Oceania",
+            928: "Oceania: Melanesia",
+            954: "Oceania: Micronesia",
+            957: "Oceania: Polynesia",
+            927: "Oceania: Australia/New Zealand",
+            900: "World",
+            901: "World: More developed regions",
+            941: "World: Least developed countries",
+            902: "World: Less developed regions",
+            934: "World: Less developed regions: excluding least developed countries",
+            948: "World: Less developed regions: excluding China"
+        }, (x) => {
+            return x.I >= 900;
+        });
+        $interval(() => {
+            Object.keys($scope.statsById).forEach((key) => {
+                updatePopulation($scope.statsById[key]);
+            });
+        }, 100);
+
+        assignCountry();
+        $scope.$on("$locationChangeSuccess", assignCountry);
+    });
+});
